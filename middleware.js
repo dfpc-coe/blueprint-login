4bf11ffa60395ea5c1613bd879fa1f46b3fbd8fd
ingalls <nick@ingalls.ca>
2022-12-30 22:04:12 -0400

    First sketch of middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,68 @@
+import Err from '@openaddresses/batch-error';
+
 /**
  * Authentication Middleware
  * @class
  *
+ * @param {Object} opts Options Object
+ * @param {String} opts.secret
+ * @param {String} opts.username
+ * @param {String} opts.password
  */
 export default class AuthenticationMiddleware {
     constructor(opts) {
-        this.opts = opts;
+        this.name = 'Login Blueprint';
+        this.secret = opts.secret;
+        this.username = opts.username;
+
+        // TODO: This is temproary for now until LDAP is set up
+        // Don't use unencrypted passwords in production - bcrypt is absolutely required
+        this.password = opts.password;
     }
 
-    auth() {
-        return async (req, res, next) => {
+    async blueprint(router, config) {
+        await router.post('/login', {
+            name: 'Create Login',
+            group: 'Login',
+            req: {
+                type: 'object',
+                required: ['username', 'password'],
+                properties: {
+                    username: {
+                        type: 'string'
+                    },
+                    password: {
+                        type: 'string'
+                    }
+                }
+            },
+            res: {
+                type: 'object',
+                required: ['token'],
+                properties: {
+                    token: {
+                        type: 'string'
+                    }
+                }
+            }
+        }, (req, res) => {
+            try {
+                if (
+                    req.body.username !== this.username
+                    || req.body.password !== this.password
+                ) {
+                    throw new Err(401, null, 'Invalid Credentials');
+                }
+
+                return res.json({
+                    token: '123'
+                });
+            } catch (err) {
+                Err.respond(err, res);
+            }
+        });
+
+        router.router.use((req, res, next) => {
             if (req.header('authorization')) {
                 const authorization = req.header('authorization').split(' ');
 
@@ -39,6 +92,6 @@ export default class AuthenticationMiddleware {
             }
 
             return next();
-        }
+        });
     }
 }
